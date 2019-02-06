c80f84d87095c06c0a5a986dab84236e95565341
JTWWilson <9078727+JTWWilson@users.noreply.github.com>
2019-02-06 19:00:41 +0000

    Dodgy board version

--- Gladiator.py
+++ Gladiator.py
@@ -34,133 +34,110 @@ GOLD_TIGER = pygame.image.load('Images/GoldTiger.png').convert()
 HIGHLIGHT_MOVE = pygame.image.load('Images/HighlightMove.png').convert_alpha()
 HIGHLIGHT_TAKE = pygame.image.load('Images/HighlightTake.png').convert_alpha()
 
+PIECE_TO_IMAGE = {
+    'be': BLACK_EMPEROR,
+    'bb': BLACK_BISHOP,
+    'bw': BLACK_SWORDSMAN,
+    'bt': BLACK_TIGER,
+    'ge': GOLD_EMPEROR,
+    'gb': GOLD_BISHOP,
+    'gw': GOLD_SWORDSMAN,
+    'gt': GOLD_TIGER,
+    '-': '-'
+}
 
-class Piece(ABC):
-    def __init__(self, x, y, allegiance):
-        self.position = [x, y]
-        self.allegiance = allegiance
-        self.selected = False
-        self.dead = False
-
-    @property
-    def x(self):
-        return self.position[0]
-
-    @property
-    def y(self):
-        return self.position[1]
-
-    def get_moves(self):
-        return ()
-
-    def get_takes(self):
-        return ()
-
-    def get_image(self):
-        return
+BLACK_PIECES = (BLACK_EMPEROR, BLACK_BISHOP, BLACK_SWORDSMAN, BLACK_TIGER)
+GOLD_PIECES = (GOLD_EMPEROR, GOLD_BISHOP, GOLD_SWORDSMAN, GOLD_TIGER)
 
 
-class Emperor(Piece):
-    def get_moves(self):
-        theoretical_moves = [(self.position[0] + x, self.position[1] + y) for y in range(0, 2) for x in range(-1, 2)]
+def get_moves(piece, board):
+    if board[piece[0]][piece[1]] == 'e':
+        theoretical_moves = [(piece[0] + x, piece[1] + y) for y in range(0, 2) for x in range(-1, 2)]
         return filter(CHECK_IN_BOARD, theoretical_moves)
 
-    def get_takes(self):
-        return self.get_moves()
-
-    def get_image(self):
-        return BLACK_EMPEROR if self.allegiance == 'BLACK' else GOLD_EMPEROR
-
-
-class Bishop(Piece):
-    def get_moves(self):
+    elif board[piece[0]][piece[1]] == 'b':
         theoretical_moves = []
         for change in range(1, BOARD_SIZE[0]):
-            theoretical_moves.append((self.position[0] + change, self.position[1] + change))
-            theoretical_moves.append((self.position[0] - change, self.position[1] + change))
-            theoretical_moves.append((self.position[0] + change, self.position[1] - change))
-            theoretical_moves.append((self.position[0] - change, self.position[1] - change))
+            theoretical_moves.append((piece[0] + change, piece[1] + change))
+            theoretical_moves.append((piece[0] - change, piece[1] + change))
+            theoretical_moves.append((piece[0] + change, piece[1] - change))
+            theoretical_moves.append((piece[0] - change, piece[1] - change))
 
         return filter(CHECK_IN_BOARD, theoretical_moves)
 
-    def get_takes(self):
-        return self.get_moves()
-
-    def get_image(self):
-        return BLACK_BISHOP if self.allegiance == 'BLACK' else GOLD_BISHOP
-
-
-class Swordsman(Piece):
-    def get_moves(self):
-        theoretical_moves = [(self.position[0] + 1, self.position[1] + 1),
-                             (self.position[0] - 1, self.position[1] + 1),
-                             (self.position[0] + 1, self.position[1] - 1),
-                             (self.position[0] - 1, self.position[1] - 1)]
+    elif board[piece[0]][piece[1]] == 's':
+        theoretical_moves = [(piece[0] + 1, piece[1] + 1),
+                             (piece[0] - 1, piece[1] + 1),
+                             (piece[0] + 1, piece[1] - 1),
+                             (piece[0] - 1, piece[1] - 1)]
         return filter(CHECK_IN_BOARD, theoretical_moves)
 
-    def get_takes(self):
-        theoretical_takes = [(self.position[0] + 1, self.position[1]),
-                             (self.position[0] - 1, self.position[1]),
-                             (self.position[0], self.position[1] + 1),
-                             (self.position[0], self.position[1] - 1)
+
+def get_takes(piece, board):
+    if board[piece[0]][piece[1]] == 'e':
+        theoretical_moves = [(piece[0] + x, piece[1] + y) for y in range(0, 2) for x in range(-1, 2)]
+        return filter(CHECK_IN_BOARD, theoretical_moves)
+
+    elif board[piece[0]][piece[1]] == 'b':
+        theoretical_moves = []
+        for change in range(1, BOARD_SIZE[0]):
+            theoretical_moves.append((piece[0] + change, piece[1] + change))
+            theoretical_moves.append((piece[0] - change, piece[1] + change))
+            theoretical_moves.append((piece[0] + change, piece[1] - change))
+            theoretical_moves.append((piece[0] - change, piece[1] - change))
+
+        return filter(CHECK_IN_BOARD, theoretical_moves)
+
+    elif board[piece[0]][piece[1]] == 's':
+        theoretical_takes = [(piece[0] + 1, piece[1]),
+                             (piece[0] - 1, piece[1]),
+                             (piece[0], piece[1] + 1),
+                             (piece[0], piece[1] - 1)
                             ]
         return filter(CHECK_IN_BOARD, theoretical_takes)
 
-    def get_image(self):
-        return BLACK_SWORDSMAN if self.allegiance == 'BLACK' else GOLD_SWORDSMAN
 
-
-class Tiger(Piece):
-    pass
-
-
-KEY = {'e': Emperor,
-       'b': Bishop,
-       'w': Swordsman,
-       't': Tiger
-       }
-
-
-def create_pieces(path, key):
-    pieces = []
+def create_board(path):
+    board = []
     with open(path) as board_file:
         for y, line in enumerate(board_file.readlines()):
+            row = []
             for x, item in enumerate(line.split(',')):
                 try:
-                    pieces.append(key[item.strip()](x, y, 'BLACK' if y == 0 else 'GOLD'))
+                    row.append(PIECE_TO_IMAGE[('b' if y == 0 else 'g' if y == BOARD_SIZE[1] - 1 else '') + item.strip()])
                 except KeyError:
                     pass
+            board.append(row)
+    return board
 
-    return pieces
 
-
-def display_board(screen, pieces):
+def display_board(screen, board, dead):
     screen.fill((0, 0, 0))
-    for y in range(BOARD_SIZE[1]):
-        for x in range(BOARD_SIZE[0]):
-            pygame.draw.rect(screen, DARK_GREY if (x + y) % 2 else LIGHT_GREY, (GRID_SIZE * x, GRID_SIZE * y, GRID_SIZE, GRID_SIZE))
+    for y, row in enumerate(board):
+        for x, item in enumerate(row):
+            if item == '-':
+                pygame.draw.rect(screen, DARK_GREY if (x + y) % 2 else LIGHT_GREY, (GRID_SIZE * x, GRID_SIZE * y, GRID_SIZE, GRID_SIZE))
+            else:
+                screen.blit(item, (GRID_SIZE * x, GRID_SIZE * y))
 
-    dead_columns = [0, 0]
-    for piece in filter(lambda x: not x.selected, pieces):
-        if piece.dead:
-            if piece.allegiance == 'BLACK':
-                screen.blit(piece.get_image(), (GRID_SIZE * (BOARD_SIZE + 2), GRID_SIZE * dead_columns[0]))
-                dead_columns[0] += 1
-            elif piece.allegiance == 'GOLD':
-                screen.blit(piece.get_image(), (GRID_SIZE * (BOARD_SIZE + 3) + MARGIN, GRID_SIZE * dead_columns[1]))
-                dead_columns[1] += 1
-        else:
-            screen.blit(piece.get_image(), (GRID_SIZE * piece.x, GRID_SIZE * piece.y))
+    for index, piece in enumerate(dead['BLACK']):
+        screen.blit(piece.get_image(), (GRID_SIZE * (BOARD_SIZE + 2), GRID_SIZE * index))
+    for index, piece in enumerate(dead['BLACK']):
+        screen.blit(piece.get_image(), (GRID_SIZE * (BOARD_SIZE + 3) + MARGIN, GRID_SIZE * index))
 
 
 if __name__ == '__main__':
     WINDOW = pygame.display.set_mode((1000, 1000))
     pygame.display.set_caption('Gladiator')
 
-    pieces = create_pieces('default_board.csv', KEY)
+    board = create_board('default_board.csv')
+    dead = {'BLACK': [], 'GOLD': []}
 
     selected = None
     selected_position = (0, 0)
+
+    display_board(WINDOW, board, dead)
+    pygame.display.flip()
 
     while True:
         for event in pygame.event.get():
@@ -168,28 +145,27 @@ if __name__ == '__main__':
                 pygame.quit()
                 quit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                display_board(WINDOW, pieces)
+                display_board(WINDOW, board, dead)
                 if event.button == 1:
-                    selected = None
-                    for piece in pieces:
-                        if pygame.Rect(piece.x * GRID_SIZE, piece.y * GRID_SIZE, GRID_SIZE, GRID_SIZE).collidepoint(event.pos[0], event.pos[1]):
-                            selected = piece
-                            selected_position = (event.pos[0] % GRID_SIZE, event.pos[1] % GRID_SIZE)
-                    if selected is not None:
-                        for move in selected.get_moves():
-                            WINDOW.blit(HIGHLIGHT_MOVE, (move[0] * GRID_SIZE, move[1] * GRID_SIZE))
-                        for take in selected.get_takes():
-                            if any(p.position == take and p.allegiance != selected.allegiance for p in pieces):
-                                WINDOW.blit(HIGHLIGHT_TAKE, (take[0] * GRID_SIZE, take[1] * GRID_SIZE))
+                    selected = (event.pos[0] // GRID_SIZE, event.pos[1] // GRID_SIZE)
+                    board[selected[0]][selected[1]] = '-'
+                    WINDOW.blit(selected.get_image(), (event.pos[0] - selected_position[0], event.pos[1] - selected_position[1]))
+                    for move in get_moves(selected, board):
+                        if board[move[0]][move[1]] == None:
+                            WINDOW.blit(HIGHLIGHT_MOVE, (GRID_SIZE * move[0], GRID_SIZE * move[1]))
+                    for take in get_takes(selected, board):
+                        if selected in GOLD_PIECES and board[take[0]][take[1]] in BLACK_PIECES or \
+                           selected in BLACK_PIECES and board[take[0]][take[1]] in GOLD_PIECES:
+                            WINDOW.blit(HIGHLIGHT_TAKE, (GRID_SIZE * take[0], GRID_SIZE * take[1]))
             elif event.type == pygame.MOUSEMOTION:
                 if selected is not None:
-                    display_board(WINDOW, filter(lambda x: x is not selected, pieces))
+                    display_board(WINDOW, board, dead)
                     WINDOW.blit(selected.get_image(), (event.pos[0] - selected_position[0], event.pos[1] - selected_position[1]))
             elif event.type == pygame.MOUSEBUTTONUP:
                 if selected is not None and event.button == 1:
                     current_tile = (event.pos[0] // GRID_SIZE, event.pos[1] // GRID_SIZE)
                     if current_tile in selected.get_takes():
-                        any([piece.position == current_tile and piece.allegiance != selected.allegiance for piece in pieces]):
+                        pass
 
                     elif current_tile in selected.get_moves():
                         selected.position = current_tile
