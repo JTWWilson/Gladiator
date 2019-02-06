6874b517eec06e62f01acf24d40ce2b24cbf0c0e
JTWWilson <9078727+JTWWilson@users.noreply.github.com>
2019-02-06 16:32:14 +0000

    Add tile highlighting for possible moves

--- Gladiator.py
+++ Gladiator.py
@@ -6,7 +6,7 @@ __author__ = 'Jacob Wilson'
 
 BOARD_SIZE = (4, 4)
 
-CHECK_IN_BOARD = lambda x, y: 0 <= x <= BOARD_SIZE[0] and 0 <= y <= BOARD_SIZE[1]
+CHECK_IN_BOARD = lambda pos: 0 <= pos[0] <= BOARD_SIZE[0] and 0 <= pos[1] <= BOARD_SIZE[1]
 
 GRID_SIZE = 100
 MARGIN = 5
@@ -28,13 +28,15 @@ GOLD_EMPEROR = pygame.image.load('Images/GoldEmperor.png').convert()
 GOLD_SWORDSMAN = pygame.image.load('Images/GoldSwordsman.png').convert()
 GOLD_TIGER = pygame.image.load('Images/GoldTiger.png').convert()
 
-
+HIGHLIGHT_MOVE = pygame.image.load('Images/HighlightMove.png').convert_alpha()
+HIGHLIGHT_TAKE = pygame.image.load('Images/HighlightTake.png').convert_alpha()
 
 
 class Piece(ABC):
     def __init__(self, x, y, allegiance):
         self.position = [x, y]
         self.allegiance = allegiance
+        self.selected = False
 
     @property
     def x(self):
@@ -133,10 +135,11 @@ def display_board(screen, pieces):
         for x in range(BOARD_SIZE[0]):
             pygame.draw.rect(screen, DARK_GREY if (x + y) % 2 else LIGHT_GREY, (GRID_SIZE * x, GRID_SIZE * y, GRID_SIZE, GRID_SIZE))
 
-    for piece in pieces:
+    for piece in filter(lambda x: not x.selected, pieces):
         screen.blit(piece.get_image(), (GRID_SIZE * piece.x, GRID_SIZE * piece.y))
 
-    pygame.display.flip()
+
+
 
 
 if __name__ == '__main__':
@@ -145,5 +148,38 @@ if __name__ == '__main__':
 
     pieces = create_pieces('default_board.csv', KEY)
 
+    selected = None
+    selected_position = (0, 0)
+
     while True:
-        display_board(WINDOW, pieces)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                display_board(WINDOW, pieces)
+                if event.button == 1:
+                    selected = None
+                    for piece in pieces:
+                        if pygame.Rect(piece.x * GRID_SIZE, piece.y * GRID_SIZE, GRID_SIZE, GRID_SIZE).collidepoint(event.pos[0], event.pos[1]):
+                            #piece.selected = True
+                            selected = piece
+                            selected_position = (event.pos[0] % GRID_SIZE, event.pos[1] % GRID_SIZE)
+                    if selected is not None:
+                        for move in selected.get_moves():
+                            WINDOW.blit(HIGHLIGHT_MOVE, (move[0] * GRID_SIZE, move[1] * GRID_SIZE))
+                        for take in selected.get_takes():
+                            if any(p.position == take for p in pieces):
+                                WINDOW.blit(HIGHLIGHT_TAKE, (take[0] * GRID_SIZE, take[1] * GRID_SIZE))
+            elif event.type == pygame.MOUSEMOTION:
+                if selected is not None:
+                    display_board(WINDOW, filter(lambda x: x is not selected, pieces))
+                    WINDOW.blit(selected.get_image(), (event.pos[0] - selected_position[0], event.pos[1] - selected_position[1]))
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if selected is not None and event.button == 1:
+                    current_tile = (event.pos[0] // GRID_SIZE, event.pos[1] // GRID_SIZE)
+                    if current_tile in selected.get_takes() or current_tile in selected.get_moves():
+                        selected.position = current_tile
+                    display_board(WINDOW, pieces)
+                    selected = None
+        pygame.display.flip()
